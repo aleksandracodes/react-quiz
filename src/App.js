824d86aa8539f53a39c2ae0796b0b87b444a003a
Aleksandra <a.haniok@gmail.com>
2024-01-11 18:39:02 +0000

    Add StartScreen component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
+import StartScreen from './StartScreen';
 
 const initialState = {
   questions: [],
@@ -29,7 +30,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ question, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
@@ -45,7 +48,7 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <div>Start screen</div>}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
