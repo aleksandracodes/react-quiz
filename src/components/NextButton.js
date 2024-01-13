4372fdc8618faa5d487a15295650cd5d18302d8a
Aleksandra <a.haniok@gmail.com>
2024-01-13 10:47:03 +0000

    Finishing the Quiz - add finish screen and adjust buttons

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,13 +1,27 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: 'nextQuestion' })}
-    >
-      Next
-    </button>
-  );
+
+  // Remove 'Next' button on the last question
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'nextQuestion' })}
+      >
+        Next
+      </button>
+    );
+
+  // Display 'Finish' button on the last question
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'finish' })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
